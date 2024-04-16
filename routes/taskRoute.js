7035799eb79269daafa5b2e65890afc30f86f06d
Rahul <rahul@adsl-ull-34-0.51-151.wind.it>
2024-04-16 18:05:40 +0530

    update validation

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -13,6 +13,28 @@ function formatDate(date) {
 
 router.post('/add', async (req, res) => {
     const { title, description, status, due_date } = req.body;
+    if (!title || !description || !status || !due_date) {
+        return res.status(400).send({
+            status: false,
+            message: 'All the fields are mandatory',
+
+        })
+    }
+    const dueDate = new Date(due_date)
+    if (dueDate <= new Date()) {
+        return res.status(400).send({
+            status: false,
+            error: 'Due date must be in the future'
+        });
+    }
+
+    if (!["pending","in-progress","completed"]?.includes(status)) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid status value'
+        });
+    }
+
     try {
         const [day, month, year] = due_date.split('-').map(Number);
         const newTask = new Task({
@@ -22,22 +44,31 @@ router.post('/add', async (req, res) => {
             due_date: new Date(year, month - 1, day)
         });
         await newTask.save();
-        res.status(201).json({ task: newTask });
+        res.status(201).send({ task: newTask });
     } catch (error) {
-        res.status(400).json({ error: 'Failed to create task' });
+        res.status(500).send({ error: 'Failed to create task' });
     }
 });
 
 
 
 router.get('/tasks', async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
     try {
-        const tasks = await Task.find();
-        const formattedTasks = tasks.map(task => ({
-            ...task.toObject(),
-            due_date: task.due_date ? formatDate(task.due_date) : null
-        }));
-        res.json(formattedTasks);
+        const totalTasks = await Task.countDocuments();
+        const totalPages = Math.ceil(totalTasks / limit);
+        const offset = (page - 1) * limit;
+        const tasks = await Task.find()
+            .skip(offset)
+            .limit(limit);
+        res.json({
+            tasks,
+            totalPages,
+            currentPage: page,
+            hasNextPage: page < totalPages,
+            hasPrevPage: page > 1
+        });
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch tasks' });
     }
